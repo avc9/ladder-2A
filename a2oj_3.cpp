902fcd85a2fb199260399a43eda679dc34917000
Akash Vivek Chodankar <63742833+avc9@users.noreply.github.com>
2020-12-17 19:47:34 +0530

    Update a2oj_3.cpp

--- a2oj_3.cpp
+++ a2oj_3.cpp
@@ -5,7 +5,7 @@ int main(){
     cin>>s1;
     transform(s1.begin(),s1.end(),s1.begin(),::tolower);
     for(auto iter=0;iter<s1.size();iter++){
-        if(s1[iter]=='a'||s1[iter]=='e'||s1[iter]=='i'||s1[iter]=='o'||s1[iter]=='u')
+        if(s1[iter]=='a'||s1[iter]=='e'||s1[iter]=='i'||s1[iter]=='o'||s1[iter]=='u'||s1[iter]=='y')
             continue;
         else
             cout<<"."<<s1[iter];
